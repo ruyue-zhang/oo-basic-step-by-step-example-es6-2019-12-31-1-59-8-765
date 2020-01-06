481ebdb353c6d08d89e45939ee4a6e4435b7f5b4
lpdft <772346730@qq.com>
2020-01-06 15:19:34 +0800

    [ruyue]-oop: spec/practice_10/main_spec

--- main/practice_10/teacher.js
+++ main/practice_10/teacher.js
@@ -1,1 +1,21 @@
+import Person from "./person.js";
 
+class Teacher extends Person {
+  constructor(id, name, age, klasses) {
+    super(id, name, age);
+    this.klasses = klasses;
+  }
+  introduce() {
+    if(this.klasses) {
+      return `${super.introduce()} I am a Teacher. I teach Class ${this.klasses.map(klasses=>klasses.number).join(', ')}.`;
+    }
+    else {
+      return `${super.introduce()} I am a Teacher. I teach No Class.`;
+    }
+  }
+  isTeaching(student) {
+    return -1 !== this.klasses.number.indexOf(student.klasses.number);
+  }
+}
+
+module.exports = Teacher;
