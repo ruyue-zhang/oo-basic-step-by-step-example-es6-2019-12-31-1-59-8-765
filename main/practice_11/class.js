6440409797d3edf6f733bd9c83f7c8e562c1135b
lpdft <772346730@qq.com>
2020-01-07 10:43:24 +0800

    [ruyue] feat-oop: spec/practice_11/main_spec

--- main/practice_11/class.js
+++ main/practice_11/class.js
@@ -1,1 +1,51 @@
+class Class {
+  constructor(number) {
+    this.number = number; 
+  }
+
+  getDisplayName() {
+    return `Class ${this.number}`;
+  }
+
+  assignLeader(student) {
+		if (student.klass.number === this.number) {
+			this.leader = student;
+    }
+    else {
+      console.log(`It is not one of us.`);
+    }
+    const leaderListener = this.leaderListener;
+    if(leaderListener) {
+      if(leaderListener.isTeaching(student)) {
+        console.log(`I am ${leaderListener.name}. I know ${student.name} become Leader of Class ${this.number}.`);
+      }
+    }
+  }
+
+  appendMember(student) {
+    student.klass = this;
+    const joinListener = this.joinListener;
+		if (joinListener) {
+			if (joinListener.isTeaching(student)) {
+				console.log(`I am ${joinListener.name}. I know ${student.name} has joined Class ${this.number}.`);
+			}
+		}
+  }
+
+  isIn(student) {
+    return student.klass.number === this.number;
+  }
+
+  registerAssignLeaderListener(teacher) {
+    this.leaderListener = teacher;
+  }
+
+  registerJoinListener(teacher) {
+    this.joinListener = teacher;
+  }
+}
+
+module.exports = Class;
+
+
 
--- main/practice_11/teacher.js
+++ main/practice_11/teacher.js
@@ -1,1 +1,26 @@
+import Person from "./person.js";
+
+class Teacher extends Person {
+  constructor(id, name, age, klasses) {
+    super(id, name, age);
+    this.klasses = klasses;
+  }
+  
+  introduce() {
+    if(this.klasses) {
+      return `${super.introduce()} I am a Teacher. I teach Class ${this.klasses.map(klasses=>klasses.number).join(', ')}.`;
+    }
+    else {
+      return `${super.introduce()} I am a Teacher. I teach No Class.`;
+    }
+  }
+
+  isTeaching(student) {
+    return this.klasses.reduce((isTeaching, elem) => {
+		  return isTeaching || elem.isIn(student);
+    }, false);
+  }
+}
+
+module.exports = Teacher;
 
